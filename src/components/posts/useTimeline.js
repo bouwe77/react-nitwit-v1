e71f611091c041c7741f04853b7ceb9a85cd9e1a
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-10-23 21:21:35 +0200

    .

--- src/components/posts/useTimeline.js
+++ src/components/posts/useTimeline.js
@@ -7,36 +7,36 @@ import settings from "../../settings";
 export default () => {
   const defaultDelay = 10000;
   const [delay, setDelay] = useState(0);
-  const [timeline, setTimeline] = useState([]);
+  const [posts, setPosts] = useState([]);
   const [etag, setEtag] = useState(null);
 
-  // Retrieve the timeline every x seconds
+  // Retrieve the posts every x seconds
   useInterval(async () => {
     // The initial delay is zero so the first API call is done right away.
     // After that, all subsequent calls must be done according to the defaultDelay.
     if (delay !== defaultDelay) setDelay(defaultDelay);
 
-    // Call the API to get the timeline.
+    // Call the API to get the posts.
     const result = await getTimelineWithEtag(settings.username, etag);
     if (result.isNew) {
-      setTimeline(result.data);
+      setPosts(result.data);
       setEtag(result.etag);
     }
   }, delay);
 
   const addPost = content => {
-    // Remember the timeline before the new post is added.
-    const prevTimeline = timeline;
+    // Remember the posts before the new post is added.
+    const prevPosts = posts;
 
     // Add new post to state BEFORE posting it to the API (i.e. "optimistic UI updates")
-    setTimeline([{ user: settings.username, content }, ...timeline]);
+    setPosts([{ user: settings.username, content }, ...posts]);
 
     // Post the new post to the API.
     savePost(settings.username, { content }).catch(() => {
       // Posting to the API failed so "rollback" the state to the previous posts.
-      setTimeline(prevTimeline);
+      setPosts(prevPosts);
     });
   };
 
-  return [timeline, addPost];
+  return [posts, addPost];
 };
