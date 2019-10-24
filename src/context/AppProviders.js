cb49e2be29ad50c2e899480b2d6b8c76abb1f2e8
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-10-24 16:12:30 +0200

    .

--- src/context/AppProviders.js
+++ src/context/AppProviders.js
@@ -1,16 +1,1 @@
-// import React from "react";
-// import { AuthProvider } from "./AuthProvider";
-// import { UserProvider } from "./UserProvider";
-
-// /**
-//  * Wraps all application wide context providers.
-//  */
-// function AppProviders({ children }) {
-//   return (
-//     <AuthProvider>
-//       <UserProvider>{children}</UserProvider>
-//     </AuthProvider>
-//   );
-// }
-
-// export default AppProviders;
+// deze klaarzetten?
